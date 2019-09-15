32d514d88509b093f3205d779a4701afed611afc
Samuel Morales Jr <smoralesjr@gmail.com>
2019-09-15 15:05:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // add solution here
-const theBeatlesPlay = function(musicians, instruments){
+const theBeatlesPlay = (musicians, instruments) =>{
   const tempArr = [];
   for (let i = 0; i < musicians.length; i++){
     tempArr.push(musicians[i] + ' ' + 'plays ' + instruments[i]);
